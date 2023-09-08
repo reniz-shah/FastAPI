bacd7d7a39315ffa408bebd58c77edf208cde716
reniz-shah <shahreniz2810@gmail.com>
2023-09-08 16:06:59 +0530

    final changes

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import os
 import json
 import struct
 
-
 FORMAT = "%(levelname)s:%(message)s"
 logging.basicConfig(format=FORMAT, level=logging.DEBUG)
 
@@ -22,47 +21,71 @@ class ReceiveDataPayloadDto(BaseModel):
     deviceId : str
     payload : list[int]
 
+class EndSessionPayloadDto(BaseModel):
+    deviceId : str
+    sessionEndTime : str
+
 
 def createDirectory(parent_dir,directory):
     path = os.path.join(parent_dir, directory)
     os.makedirs(path)
 
+def get_subfolders(folder_path):
+    subfolders = []
+    if os.path.exists(folder_path):
+        with os.scandir(folder_path) as entries:
+            for entry in entries:
+                if entry.is_dir():
+                    subfolders.append(entry.name)
+    return subfolders
+
+def get_files_in_folder(folder_path):
+    txt_files = []
+    if os.path.exists(folder_path):
+        with os.scandir(folder_path) as entries:
+            for entry in entries:
+                if entry.is_file() and entry.name.endswith('.csv') and not entry.name.endswith('BR.csv'):
+                    txt_files.append(entry.name)
+    return txt_files
+
+def get_subfolders_and_files(folder_path):
+    txt_files = []
+    
+    if os.path.exists(folder_path):
+        for root, _, files in os.walk(folder_path):
+            for file in files:
+                if file.endswith('.bin'):
+                    txt_files.append(os.path.join(root, file))
+    
+    return txt_files
+
+
 @app.get("/")
 def root():
-    return {"message" : "App running successfully"}
+    return {"message" : "Server running successfully"}
 
-@app.post("/sendData")
-def sendData(requestData:ReceiveDataPayloadDto):
+
+@app.post("/startSession")
+def startSession(requestData:HeaderPayloadDto):
     logging.info(f'/sendData called with payload : {requestData}')
-    currentDir = os.getcwd()
-    fn = os.path.join(currentDir, 'filesData.json')
-    filesData = json.loads(open(fn).read())
-    fileName = filesData[requestData.deviceId]['latest']
-    file = open(fileName,"ab")
-    logging.info(f'/Writing data to file {fileName}')
-    byte_list = struct.pack(f'{len(requestData.payload)}B', *requestData.payload)
-    file.write(byte_list)
-    file.close()
-    return {'message' : 'Successfully added data to file!!'}
-
-
-@app.post("/sendHeader")
-def sendHeader(requestData:HeaderPayloadDto):
-    logging.info(f'/sendData called with payload : {requestData}')
-    currentDir = os.getcwd()
+    currentDir = os.path.join(os.getcwd(),"allUsers")
     child_dir = os.path.join(currentDir, requestData.userId)
     fn = os.path.join(currentDir, 'filesData.json')
     filesData = dict(json.loads(open(fn).read()))
     filePath = os.path.join(child_dir, requestData.deviceId)
     fileName = os.path.join(filePath, (str(requestData.sessionStartTime).replace(':','-').replace('.','-') + ".bin"))
 
+    
+    logging.info(f'Creating directories for user and device')
     try:
-        logging.info(f'Creating directories for user and device')
         createDirectory(currentDir,requestData.userId)
+    except:
+        pass
+    try:
         createDirectory(child_dir,requestData.deviceId)
         filesData[requestData.deviceId] = {'latest' : fileName, 'others' : []}
     except Exception as e:
-        return {"error" : e}
+        pass
     
     if not os.path.isfile(fileName):
         logging.info(f'New sessionStartTime received')
@@ -79,18 +102,59 @@ def sendHeader(requestData:HeaderPayloadDto):
     file.close()
     return {'message' : 'File created Successfully!!'}
 
-@app.get("/getOutput")
-def getOutput(deviceId:str):
-    logging.info(f'/getOutput called with deviceId : {deviceId}')
-    currentDir = os.getcwd()
+
+@app.post("/ongoingSession")
+def ongoingSession(requestData:ReceiveDataPayloadDto):
+    logging.info(f'/sendData called with payload : {requestData}')
+    currentDir = os.path.join(os.getcwd(),"allUsers")
     fn = os.path.join(currentDir, 'filesData.json')
     filesData = json.loads(open(fn).read())
-    fileName = filesData[deviceId]['latest']
+    fileName = filesData[requestData.deviceId]['latest']
+    file = open(fileName,"ab")
+    logging.info(f'/Writing data to file {fileName}')
+    byte_list = struct.pack(f'{len(requestData.payload)}B', *requestData.payload)
+    file.write(byte_list)
+    file.close()
+    return {'message' : 'Successfully added data to file!!'}
+
+
+@app.post("/endSession")
+def endSession(payload : EndSessionPayloadDto):
+    logging.info(f'/endSession called with deviceId : {payload.deviceId}')
+    payload.sessionEndTime = payload.sessionEndTime.replace(':','-').replace('.','-')
+    currentDir = os.path.join(os.getcwd(),"allUsers")
+    fn = os.path.join(currentDir, 'filesData.json')
+    filesData = json.loads(open(fn).read())
+    fileName = filesData[payload.deviceId]['latest']
+    file = fileName.split('/')[-1].split('.')[0]
     path = os.path.dirname(fileName)
+    logging.info("converting bin to ascii and hr")
+    os.system(f"python3 VS_ppg_proc_v004_20230907_1930.py -i {path} -o {path} -a /home/ubuntu/data/vsstreamcodec -f False")
+    logging.info("ascii to bin converted successfully")
+    logging.info("generating breath rate")
+    os.system(f"python3 breath_darshan.py {path}/output/result_PPI_{file}.txt {path} {path}/{payload.deviceId}_BR.csv")
+    os.system(f"python3 HR.py --input {path}/output/result_HR_HRV_{file}.txt --output {path}/{payload.deviceId}_{file}_TO_{payload.sessionEndTime}.csv --bin {fileName} --breath {path}/{payload.deviceId}_BR.csv")
+    logging.info("breath rate generated successfully")
+    logging.info(f'Sending csv file at path: {path} with name : {payload.deviceId}.csv')
+    return FileResponse(os.path.join(path, f"{payload.deviceId}_{file}_TO_{payload.sessionEndTime}.csv") )
 
-    logging.info(f'Sending csv file at path: {path} with name : {deviceId}.csv')
-    return FileResponse(os.path.join(path, deviceId+".csv") )
-
-@app.get("/getAllDevicesByUserId")
+@app.get("/{userId}/devices")
 def getAllDevices(userId : str):
-    return {'userId' : userId}
+    currentDir = os.path.join(os.getcwd(),"allUsers")
+    folder_path = os.path.join(currentDir, userId)
+    subfolders = get_subfolders(folder_path)
+    return subfolders
+
+@app.get("/{userId}/{deviceId}/recordings")
+def getAllRecordings(userId : str, deviceId : str):
+    currentDir = os.path.join(os.getcwd(),"allUsers")
+    folder_path = os.path.join(currentDir, userId)
+    folder_path = os.path.join(folder_path, deviceId)
+    subfolders = get_files_in_folder(folder_path)
+    return subfolders
+
+@app.get("/{userId}/{deviceId}/{recordingId}")
+def getRecordingByUserIdAndDeviceId(userId : str, deviceId : str, recordingId:str):
+    currentDir = os.path.join(os.getcwd(),"allUsers")
+    filePath = os.path.join(currentDir, f"{userId}/{deviceId}/{recordingId}")
+    return FileResponse(filePath)
